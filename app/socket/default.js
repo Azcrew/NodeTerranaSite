96e9485159a597644e382458a9183a98b9825a0b
Bruno J. Spolavori <bspol.323@gmail.com>
2018-08-10 05:36:15 -0300

    socket update

--- app/socket/default.js
+++ app/socket/default.js
@@ -1,13 +1,13 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        console.log('User ' + socket.id + ' Connected on Socket')
+        // console.log('User ' + socket.id + ' Connected on Socket')
 
         socket.on('', (socket) => {
 
         })
 
         socket.on('disconnect', (socket) => {
-            console.log('User ' + socket.id + ' Disconnected from Socket')
+            // console.log('User ' + socket.id + ' Disconnected from Socket')
         })
     })
 }
--- app/socket/messenger.js
+++ app/socket/messenger.js
@@ -1,14 +1,10 @@
 module.exports = (io) => {
     var messenger = io.of('/messenger')
     messenger.on('connection', (socket) => {
-        console.log('User ' + socket.id + ' Connected Messenger')
-
         socket.on('', (socket) => {
-
         })
 
         socket.on('disconnect', (socket) => {
-            console.log('User ' + socket.id + ' Disconnected from Socket')
         })
     })
 }
