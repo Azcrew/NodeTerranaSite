53210fcf2cbebf7896cae37124107f7893a1c9bc
Bruno J. Spolavori <bspol.323@gmail.com>
2018-08-10 04:15:43 -0300

    add messenger sidebar

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,10 +1,17 @@
 $(document).ready(() => {
-    var socket = io.connect('https://' + window.location.hostname + ':443')
+    $('#sidebar-toggle-a').on('click', function () {
+        $('#sidebar').toggleClass('active')
+    })
+    $('#sidebar-toggle-b').on('click', function () {
+        $('#sidebar').toggleClass('active')
+    })
 
+    var socket = io.connect('https://' + window.location.hostname + ':443')
     socket.on('disconnect', () => {
         // ;D Temp code to simp view
         setTimeout(() => {
             location.reload()
         }, 1500)
     })
+
 })
